5389c27a00f9cc985d4c4707117856b026fb62a1
wavyStructures <anja_schwab@gmx.de>
2024-12-31 07:18:16 +0100

    user_auth customLoginView

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -126,7 +126,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
@@ -135,3 +134,4 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
 }
+
--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -1,6 +1,11 @@
 from rest_framework import generics
 from user_auth_app.models import UserProfile
-from .serializers import UserProfileSerializer
+from .serializers import UserProfileSerializer, RegistrationSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken  
 
 class UserProfileList(generics.ListCreateAPIView):
     queryset = UserProfile.objects.all()
@@ -9,3 +14,42 @@ class UserProfileList(generics.ListCreateAPIView):
 class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+    
+class RegistrationView(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            saved_account = serializer.save()
+            token, created = Token.objects.get_or_create(user=saved_account)
+            data = {
+                'token': token.key,
+                'username': saved_account.username,
+                'email': saved_account.email,
+            }
+        else:
+            data = serializer.errors 
+ 
+        return Response(data)
+
+class CustomLoginView(ObteinAuthToken):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        serializer = self.serializer(data=request.data)
+        
+        if serializer.is_valid():
+            user = serializer.validated_data['user'] q
+            token, created = Token.objects.get_or_create(user=user)
+            data = {
+                'token': token.key,
+                'username': saved_account.username,
+                'email': saved_account.email,
+            }
+        else:
+            data = serializer.errors 
+ 
+        return Response(data)
+  
